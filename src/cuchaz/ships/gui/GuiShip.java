be96863e9888c6047c9ae7922f92ee09122857a9
jeff <devnull@localhost>
2013-11-05 09:53:23 -0500

    added GPL license

--- src/cuchaz/ships/gui/GuiShip.java
+++ src/cuchaz/ships/gui/GuiShip.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Jeff Martin.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Jeff Martin - initial API and implementation
+ ******************************************************************************/
 package cuchaz.ships.gui;
 
 import static cuchaz.ships.gui.GuiSettings.LeftMargin;
